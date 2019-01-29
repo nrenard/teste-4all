525a39916771d2c93abbe6cf7d392295a359d488
Nicolas <nicolasrenard1999@gmail.com>
2019-01-29 18:34:18 -0200

    Adicionado Header no front-end.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import { GlobalStyle } from "./styles/global";
+
+import Header from './components/Header';
 
 import Routes from './routes/index';
 
 const App = () => (
-  <div className="App">
-    <Routes />
-  </div>
+	<>
+		<GlobalStyle />
+		<div className="App">
+			<Header />
+			<Routes />
+		</div>
+	</>
 );
 
 export default App;
