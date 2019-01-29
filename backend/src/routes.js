f42f3b9b6329662be4b2f4d50050b24924a51385
Nicolas Renard <nicolasrenard1999@gmail.com>
2019-01-28 22:11:54 -0200

    Iniciando o backend.

---
+++ backend/src/routes.js
@@ -0,0 +1,5 @@
+const routes = require('express').Router()
+
+routes.get('/', (req, res) => res.json({ aeee: "aeae" }))
+
+module.exports = routes
---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+class Server {
+  constructor() {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+
+module.exports = new Server().express
